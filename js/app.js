cf8e381bc8bbeb9b71b15997889a4f2a1cc27b5a
TrevTheCat <alicegdunn@hotmail.co.uk>
2015-10-11 15:20:33 +0100

    basic 1 square battleships game working

--- js/app.js
+++ js/app.js
@@ -1,35 +1,47 @@
 $(function(){
-setup()
-
+  setup();
 })
 
 function setup(){
-  $("td").on("click", function(){
+  $("td.box").on("click", function(){
     $(this).toggleClass("placedShip");
+    var select = Math.ceil(Math.random()*9);
+    $("td#c"+select+".comp").toggleClass("computerShip");
+    $("td").off("click");  
+ playerMove()
+  })
   
-  
-  var select = Math.floor(Math.random()*9);
-  var compShip = $(".box["+select+"]");
-  $(".box[5]").toggleClass("computerShip");
-})
-  playGame();
-
-}
-
-function playGame (){
-computerMove();
-playerMove();
-
 }
 
 function playerMove(){
-
-getWinner()
+  $("h2").text("guess where the computer's ship is on the computer board!");
+  $("td").on("click", function(){
+    event.target.style.backgroundColor = 'yellow';
+    playerGo= $(event.target);
+    console.log(playerGo.attr("id"))
+    if ($(event.target).hasClass("computerShip")){
+      $("h2").text("Player hit, you Win!");
+      return
+  }
+    else {
+    console.log("miss");
+    computerMove();
   }
 
-function computerMove(){
+})
+}
 
-getWinner()
+function computerMove(){
+  var computerChoose = Math.ceil(Math.random()*9);
+  $("td#p"+computerChoose+".box");
+  if ($("td#p"+computerChoose+".box").hasClass("placedShip")){
+    $("h2").text("Computer Hit you! Computer Wins!")
+  }
+  else {
+$("h2").text("You Missed! Computer chose "+computerChoose+"and missed. Try again")
+  $("td#p"+computerChoose+".box").style.backgroundColor='green';
+  playerMove ()
+  }
 }
 
 function getWinner(){
